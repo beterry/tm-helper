6a6d327287a687fd43f20fd8181717dd192538e5
Ben Terry <benterry.dev@gmail.com>
2023-07-24 17:05:02 -0400

    cube touch animation

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,15 +1,24 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 // constants
-import { COLORS } from '../constants'
+import { COLORS } from '../constants';
 
 const Cube = ({color, isTouched, handleCubeTouched, index, value, resource}) => {
+    const [mouseDown, setMouseDown] = React.useState(false);
+
     return (
         <CubeWrapper 
             color={color} 
             onClick={() => handleCubeTouched(index)}
             touched={isTouched}
             aria-label={value + ' ' + resource}
+            onMouseDown={() => setMouseDown(true)}
+            onTouchStart={() => setMouseDown(true)}
+            onMouseLeave={() => setMouseDown(false)}
+            onTouchEnd={() => setMouseDown(false)}
+            onMouseUp={() => setMouseDown(false)}
+            mouseDown={mouseDown}
         />
     )
 }
@@ -21,6 +30,8 @@ const CubeWrapper = styled.button`
     border-radius: 4px;
     border: none;
     opacity: ${props => props.touched ? .3 : 1};
+    transform: scale(${props => props.mouseDown ? '0.9' : '1'});
+    transition: transform ${props => props.mouseDown ? '50ms' : '200ms'};
 `
 
 export default Cube;
